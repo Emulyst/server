90b14ea74aabfaec449267245ed50ad9f6c13476
Kaamil Jasani <kaamilj@yahoo.com>
2020-04-12 19:27:07 +0100

    Removed admin stuff

--- src/responses/login.js
+++ src/responses/login.js
@@ -5,10 +5,6 @@ const serviceKey = require('../serviceAccountKey.json');
 
 firebase.initializeApp(firebaseConfig);
 
-admin.auth().getUserByEmail("test@test.com").then(user => {
-    admin.auth().setCustomUserClaims(user.uid, {username: "Testing123", id: "testuser"}); 
-});
-
 router.addStaticRoute("/session/bnea_login", async (url, data)=>{
     let credentials = json.fromBuffer(data);
     let {user} = await firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password);
